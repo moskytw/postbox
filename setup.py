1264c58db4cc36432a6393fa8855d6872f6620c4
Mosky <mosky.tw@gmail.com>
2013-06-25 22:55:17 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+from postbox import __version__
+
+setup(
+    name    = 'postbox',
+    description = 'It makes sending emails easier with Python.',
+    long_description = open('README.rst').read(),
+    version = __version__,
+    author  = 'Mosky',
+    author_email = 'mosky.tw@gmail.com',
+    #url = 'http://postbox.mosky.tw/',
+    py_modules = ['postbox'],
+    license = 'MIT',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
+
