a003a25ad7fa06d14b7de01b2d95f4e13f871688
Mosky <mosky.tw@gmail.com>
2013-06-25 11:20:04 +0800

    add examples

---
+++ postbox_examples.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from postbox import Gmail
+
+with Gmail(user='mosky.bot@gmail.com') as gmail:
+
+    gmail.send(
+        from_   = 'Mosky Bot <mosky.bot@gmail.com>',
+        to      = 'Mosky Liu <mosky.tw@gmail.com>',
+        subject = 'Test from Postbox :) #simplest',
+        body    = ':D',
+    )
+
+    gmail.send(
+        from_   = 'Mosky Bot <mosky.bot@gmail.com>',
+        to = [
+            'Mosky Liu <mosky.tw@gmail.com>',
+            'Mosky Liu <mosky.liu@pinkoi.com>',
+            'Mosky Liu <mosky@ubuntu-tw.org>'
+        ],
+        cc = [
+            'Mosky Liu <mosky.tw@gmail.com>',
+            'Mosky Liu <mosky.liu@pinkoi.com>',
+            'Mosky Liu <mosky@ubuntu-tw.org>'
+        ],
+        subject = 'Test from Postbox :) #multi-to-cc',
+        body    = ':D',
+    )
+
+    gmail.send(
+        from_   = 'Mosky Bot <mosky.bot@gmail.com>',
+        to      = 'Mosky Liu <mosky.tw@gmail.com>',
+        reply_to = 'Mosky Bot <mosky.bot+123@gmail.com>',
+        subject = 'Test from Postbox :) #reply-to',
+        body    = ':D',
+    )
